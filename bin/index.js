af0a955559a408545f1e70a609d2206d70ef18c6
Frank_lyan <yanlee26@users.noreply.github.com>
2018-12-06 18:10:05 +0800

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+/*@yanlee26*/
+
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 typeof define === 'function' && define.amd ? define(factory) : (global.Qarticles = factory());
